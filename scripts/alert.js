abebb86be161c3ebee52d64f5a2038cd87e0e761
BohdanBest <bohdanbohdan32@gmail.com>
2025-05-24 13:59:45 +0300

    complete lab2

---
+++ scripts/alert.js
@@ -0,0 +1,7 @@
+if (typeof person !== "undefined" && person != null) {
+  alert(
+    "Як Ваші справи, " + person + "? \n Ви готові для нашої подальшої роботи?"
+  );
+} else {
+  alert("Ви не ввели ім'я. Готові до роботи?");
+}
---
+++ scripts/bulb.js
@@ -0,0 +1,6 @@
+function turnOn() {
+  document.getElementById("bulb").classList.add("bulb-on");
+}
+function turnOff() {
+  document.getElementById("bulb").classList.remove("bulb-on");
+}
